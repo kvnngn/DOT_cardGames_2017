c2d3c77cbfc6a8ad6c5bed0bfb28b510520d22a4
Nguyen Kévin <kevin3.nguyen@epitech.eu>
2019-02-11 14:33:54 +0100

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Tape your message :");
+                string msg = Console.ReadLine();
+                Console.WriteLine("Sending message to server saying '" + msg + "'");
+                NetworkComms.SendObject("Message", "127.0.0.1", 4242, msg);
+                if (Console.ReadKey(true).Key == ConsoleKey.Q) break;
+            }
+            NetworkComms.Shutdown();
+        }
+    }
+}
