468e45fd91af1cca390e2cb6f72a2f8584f3a5e0
Nguyen Kévin <kevin3.nguyen@epitech.eu>
2019-02-11 14:34:57 +0100

    Add files via upload

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 4242));
+            Console.WriteLine("\nPress any key to close server.");
+            Console.ReadKey(true);
+            NetworkComms.Shutdown();
+        }
+
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine(connection.ToString() + " said '" + message + "'.");
+        }
+    }
+}
